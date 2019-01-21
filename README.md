c5840cef22d4ef5bc432dcdcc7143598741c61a8
sechun <sechun0215@gmail.com>
2019-01-21 20:51:15 +0900

    build script

--- README.md
+++ README.md
@@ -8,15 +8,16 @@
 - Hibernate
 - H2 DataBase(Temporary)
 - Handlebars
+- WebSocket(Sock.js)
 
 ---
 
 ### Requirement
 
 1. 사용자 관리
-    - 로그인(추후에 Spring-Security 쓸 것)
+    - 로그인
     - 프로필 사진 등록
 2. IndianPoker Game
-    - auto gamer와 게임
+    - 두 명의 Player가 동시에 게임을 실행
     - 게임 결과 DB에 사용자별로 등록
 3. 사용자 랭킹 조회
---
+++ auto.sh
@@ -0,0 +1,34 @@
+#! /bin/bash
+APPLICATION_NAME=IndianPoker_Spring-Boot
+
+SCRIPTPATH=$( cd "$(dirname "$0")" ; pwd -P )
+echo "sh파일 실행 위치 $SCRIPTPATH"
+
+echo "> Git pull"
+
+git pull
+
+echo "> Build project"
+
+./gradlew build
+
+CURRENT_PID=$(pgrep -f indianpokerspring)
+
+echo "$CURRENT_PID"
+
+if [ -z $CURRENT_PID ]; then
+  echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
+else
+  echo "> kill -2 $CURRENT_PID"
+  kill -2 $CURRENT_PID
+  sleep 5
+fi
+
+echo "> 새 어플리케이션 배포"
+
+JAR_NAME=$(ls $SCRIPTPATH/build/libs |grep 'indianpokerspring' | tail -n 1)
+
+echo "> JAR Name: $JAR_NAME"
+
+chmod 755 $SCRIPTPATH/build/libs/$JAR_NAME
+nohup java -jar $SCRIPTPATH/build/libs/$JAR_NAME &
